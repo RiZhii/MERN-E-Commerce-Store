5948aaa4822c53146afe7f5f5a59175e2c81622b
RiZhii <dhuruvarishi7@gmail.com>
2026-06-22 14:23:39 +0530

    new changes

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -55,7 +55,7 @@ const Navigation = () => {
           className="flex items-center transition-transform transform hover:translate-x-2"
         >
           <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
-          <span className="hidden nav-item-name mt-[3rem]">START</span>{" "}
+          <span className="hidden nav-item-name mt-[3rem]">HOME</span>{" "}
         </Link>
 
         <Link
